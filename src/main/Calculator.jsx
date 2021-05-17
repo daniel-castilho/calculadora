a9d5c3c17a7fe57ba2228113a149d85f0e2a3113
Daniel Castilho <dan.castilho@gmail.com>
2021-05-17 17:10:48 -0400

    Implementing Logic #02

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -18,6 +18,7 @@ export default class Calculator extends Component {
 
 	constructor(props) {
 		super(props);
+
 		this.clearMemory = this.clearMemory.bind(this);
 		this.setOperation = this.setOperation.bind(this);
 		this.addDigit = this.addDigit.bind(this);
@@ -28,7 +29,30 @@ export default class Calculator extends Component {
 	}
 
 	setOperation(operation) {
-		console.log(operation);
+		if (this.state.current === 0) {
+			this.setState({ operation, current: 1, clearDisplay: true });
+		} else {
+			const equals = operation === "=";
+			const currentOperation = this.state.operation;
+
+			const values = [...this.state.values];
+			try {
+				values[0] = eval(
+					`${values[0]} ${currentOperation} ${values[1]}`
+				);
+			} catch (e) {
+				values[0] = this.state.values[0];
+			}
+			values[1] = 0;
+
+			this.setState({
+				displayValue: values[0],
+				operation: equals ? null : operation,
+				current: equals ? 0 : 1,
+				clearDisplay: !equals,
+				values,
+			});
+		}
 	}
 
 	addDigit(n) {
@@ -48,7 +72,7 @@ export default class Calculator extends Component {
 			const values = [...this.state.values];
 			values[i] = newValue;
 			this.setState({ values });
-            console.log(values);
+			console.log(values);
 		}
 	}
 
